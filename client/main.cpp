320b08ea2a9d31a55e3de9856fe6a125647fbd9e
Isaac Gentz <ikegentz@rams.colostate.edu>
2018-10-22 21:36:37 -0600

    Started on P1

---
+++ client/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by ikegentz on 10/22/18.
+//
+
+int main(int argc, char **argv)
+{
+    // use getopt to parse the command line arguments
+    //-h hostname
+    //-u username
+    //-p server port
+    //-c configuarion file
+    //-t run test file; -t test2.txt
+    //-L log_file_name (for log messages)
+}
