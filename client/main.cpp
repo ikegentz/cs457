0906049b8534e65583f39d2b8f12057185c716cd
Isaac Gentz <ikegentz@rams.colostate.edu>
2018-10-25 11:43:58 -0600

    Got options with getopt for client. Going to start on server so I know what the heck the client has to connectt to

--- client/main.cpp
+++ client/main.cpp
@@ -2,13 +2,103 @@
 // Created by ikegentz on 10/22/18.
 //
 
+#include <unistd.h>
+#include <iostream>
+
+namespace IRC_Client
+{
+    const std::string DEFAULT_HOSTNAME = "localhost";
+    const std::string DEFAULT_USERNAME = "chatterbox-charlie";
+    const unsigned DEFAULT_SERVER_PORT = 1997;
+    const std::string DEFAULT_LOGPATH = "client.log";
+
+    std::string usage()
+    {
+        std::string ret = "Connect to an IRC chat server.\n\n";
+        ret += "USAGE: client [options]\n";
+        ret += "OPTIONS\n";
+
+        ret += "\t-h HOSTNAME\n";
+        ret += "\t\t Hostname of server to connect to\n";
+        ret += "\t\t Default is " + DEFAULT_HOSTNAME + "\n";
+
+        ret += "\t-u USERNAME\n";
+        ret += "\t\t Username to connect as (i.e. your username)\n";
+        ret += "\t\t Default is " + DEFAULT_USERNAME + "\n";
+
+        ret += "\t-p SERVER_PORT\n";
+        ret += "\t\t Port on server to connect to\n";
+        ret += "\t\t Default will be loaded from server config file, or "+ std::to_string(DEFAULT_SERVER_PORT) + " if no config file\n";
+
+        ret += "\t-c CONFIG_FILEPATH\n";
+        ret += "\t\t File with configuration options\n";
+        ret += "\t\t If none specified, default options will be used\n";
+
+        ret += "\t-t TEST_FILEPATH\n";
+        ret += "\t\t Test file to run sample commands\n";
+        ret += "\t\t If none specified, will run in interactive mode\n";
+
+        ret += "\t-L LOG_FILEPATH\n";
+        ret += "\t\t File to write logs\n";
+        ret += "\t\t Defaults to " + DEFAULT_LOGPATH + "\n";
+
+        return ret;
+    }
+
+    std::string err_bad_arg(char selected)
+    {
+        std::string ret = "UNRECOGNIZED PROGRAM ARGUMENT: -" + selected;
+        return ret;
+    }
+
+}
 int main(int argc, char **argv)
 {
-    // use getopt to parse the command line arguments
-    //-h hostname
-    //-u username
-    //-p server port
-    //-c configuarion file
-    //-t run test file; -t test2.txt
-    //-L log_file_name (for log messages)
+    if(argc == 1)
+        std::cout << "Running with default options" << std::endl;
+
+    int opt;
+    char selected_opt = '.';
+    char* arg;
+
+    while ((opt = getopt(argc, argv, "h:u:p:c:t:L:")) != -1)
+    {
+        switch(opt) {
+            case 'h':
+                selected_opt = 'h';
+                arg = optarg;
+                break;
+            case 'u':
+                selected_opt = 'u';
+                arg = optarg;
+                break;
+            case 'p':
+                selected_opt = 'p';
+                arg = optarg;
+                break;
+            case 'c':
+                selected_opt = 'c';
+                arg = optarg;
+                break;
+            case 't':
+                selected_opt = 't';
+                arg = optarg;
+                break;
+            case 'L':
+                selected_opt = 'L';
+                arg = optarg;
+                break;
+            case 'H':
+                selected_opt = 'H';
+                std::cout << IRC_Client::usage() << std::endl;
+                exit(0);
+            default:
+                std::cerr << IRC_Client::err_bad_arg(selected_opt) << std::endl;
+                std::cout << IRC_Client::usage() << std::endl;
+                exit(1);
+        }
+    }
+
+    std::cout << "Starting the client..." << std::endl;
+
 }
